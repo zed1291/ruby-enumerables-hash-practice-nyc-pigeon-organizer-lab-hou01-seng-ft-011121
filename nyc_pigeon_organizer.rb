b9d512f0c7f3e9748eb173905f18fdd8a564d1b1
zed <zed@kolk.me>
2021-01-01 22:20:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,5 +8,28 @@ def nyc_pigeon_organizer data
     pigeon_list[pigeon_name] = {color: [], gender: [], lives: []}
   end
 
+  data[:color].each do |color, names|
+    # {
+    #   :purple => ["Theo", "Peter Jr.", "Lucky"],
+    #   :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    #   :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    #   :brown => ["Queenie", "Alex"]
+    # }
+    # pigeon_list["Theo"][:color] = ["purple", "grey"]
+
+    # puts color
+    # puts names
+    names.each { |name| pigeon_list[name][:color] << color}
+
+  end
+
+  data[:gender].each do |genders, names|
+    names.each { |name| pigeon_list[name][:gender] << genders}
+  end
+
+  data[:lives].each do |locations, names|
+    names.each { |name| pigeon_list[name][:lives] << locations}
+  end
+
   return pigeon_list
 end
