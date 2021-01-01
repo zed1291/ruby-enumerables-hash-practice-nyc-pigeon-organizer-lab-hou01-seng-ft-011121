cdb3bec6873ae484c2b9a65778cfcb084a3e1cde
zed <zed@kolk.me>
2021-01-01 22:38:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer data
+  all_pigeon_names = data[:gender][:male]
+  all_pigeon_names += data[:gender][:female]
+
+  pigeon_list = {}
+
+  all_pigeon_names.map do |pigeon_name|
+    pigeon_list[pigeon_name] = {color: [], gender: [], lives: []}
+  end
+
+  data[:color].each do |color, names|
+    names.each { |name| pigeon_list[name][:color] << "#{color}"}
+
+  end
+
+  data[:gender].each do |genders, names|
+    names.each { |name| pigeon_list[name][:gender] << "#{genders}"}
+  end
+
+  data[:lives].each do |locations, names|
+    names.each { |name| pigeon_list[name][:lives] << "#{locations}"}
+  end
+
+  pigeon_list
 end
