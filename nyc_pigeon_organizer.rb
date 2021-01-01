8cbc533f5797ac6b954e539f38fa05a400f3c8ca
zed <zed@kolk.me>
2021-01-01 21:05:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer data
+  all_pigeon_names = data[:gender][:male]
+  all_pigeon_names += data[:gender][:female]
+
+  pigeon_list = {}
+
+  all_pigeon_names.map do |pigeon_name|
+    pigeon_list[pigeon_name] = {color: [], gender: [], lives: []}
+  end
+
+  return pigeon_list
 end
